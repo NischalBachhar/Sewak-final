c22a0c65bbbd239f8cabad37d9c438eda0ab03ef
Nischal Bachhar <109014808+NischalBachhar@users.noreply.github.com>
2026-09-23 09:36:35 +0545

    mobile: add app/browse.tsx

---
+++ mobile/app/browse.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Screen } from "@/components/Screen";
+import { CaregiverDirectory } from "@/components/CaregiverDirectory";
+
+export default function BrowseScreen() {
+  return (
+    <Screen>
+      <CaregiverDirectory />
+    </Screen>
+  );
+}
